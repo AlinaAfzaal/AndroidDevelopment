540554682e46da03e74046011d1ce67082b015b7
AlinaAfzaal <alinaafzaalhussain@gmail.com>
2023-10-17 04:21:01 -0700

    Home page is added

--- app/src/main/java/com/example/productcatelog/LoginActivity.java
+++ app/src/main/java/com/example/productcatelog/LoginActivity.java
@@ -6,22 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.TextureView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView signupLink;
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         signupLink = findViewById(R.id.signupLink);
+        loginBtn = findViewById(R.id.loginBtn);
 
 
         signupLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
             }
         });
 
